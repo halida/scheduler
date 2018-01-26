212ca8c7e6997b8e686929d2dc69209d833fe291
halida <linjunhalida@gmail.com>
2018-01-26 15:48:21 +0800

    add usage

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ Scheduler is such a tool.
 - Run server:
     - `make run`
     - Goto http://localhost:8080
+
+## Usage
+
+- Please check [scheduler_test.rb](https://github.com/halida/scheduler/blob/master/test/models/scheduler_test.rb) to see examples.
+- The configuration step is like this:
+    - Create new plan, assign plan token (used for API call)
+    - Create routine for this plan, format is similar with crontab.
+- Scheduler will auto create execution by routine configuration.
+    - When the time come, the execution status will change from initialize to running.
+    - If Scheduler receive a API request for this execution, the status will change to succeeded.
+    - If reach timeout, the status will change to timeout, and send email to notify user.
+- If you want to send Notify about the result, goto your worker project, use [SchedulerApi](https://github.com/halida/scheduler/blob/master/lib/scheduler_api.rb) to send execution result.
--- lib/scheduler_api.rb
+++ lib/scheduler_api.rb
@@ -1,9 +1,46 @@
 class SchedulerApi
   class << self
-
-    def send_result(execution_id, result)
-      todo
+    def config
+      OpenStruct.new(
+        domain: 'scheduler.dev',
+        protocol: 'http',
+        plans: {
+          test_plan: 'token',
+        }
+      )
     end
 
+    def post(url, params)
+      RestClient.post(url, params)
+    end
+
+    def notify_execution(token, status, result)
+      self.post("#{config.protocol}://#{config.domain}/api/executions/#{token}/notify",
+                status: status, result: result.to_json)
+    end
+
+    def notify_plan(token, status, result)
+      self.post("#{config.protocol}://#{config.domain}/api/plans/#{token}/notify",
+                status: status, result: result.to_json)
+    end
+
+    # Usage:
+    # SchedulerApi.notify_plan_with_name('test_plan', :succeeded, result)
+    def notify_plan_with_name(name, status, result={})
+      return unless (
+          config = self.config and
+          plans = self.config.plans || {}
+          token = plans[name.to_sym]
+        )
+      File.open(File.join(Rails.root, "log/scheduler_api.log"), 'a+') do |f|
+        f << "#{Time.now} #{name} #{status} #{result}\n"
+      end
+      result = {
+        name: name,
+        finished_at: Time.now,
+        result: result,
+      }
+      SchedulerApi.notify_plan(token, status, result)
+    end
   end
 end
