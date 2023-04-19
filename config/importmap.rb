903bf04e4e46af0c0f31506b948793483fc164a1
halida <linjunhalida@gmail.com>
2023-04-19 22:24:02 +0800

    working on importmap

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "magnific-popup", to: "https://ga.jspm.io/npm:magnific-popup@1.1.0/dist/jquery.magnific-popup.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.4/dist/jquery.js"
+pin "select2", to: "https://ga.jspm.io/npm:select2@4.1.0-rc.0/dist/js/select2.js"
+pin "bootstrap-datepicker", to: "https://ga.jspm.io/npm:bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.js"
