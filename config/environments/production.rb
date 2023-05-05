4bedd486e257298ac04fa7d29e04e165ec114b11
halida <linjunhalida@gmail.com>
2023-05-05 15:20:57 +0800

    fix send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  if smtp_config = Settings[:smtp_settings]
+    config.action_mailer.smtp_settings = smtp_config.symbolize_keys
+    # can disable email delivery in config, like:
+    #   send_email: false
+    config.action_mailer.perform_deliveries = smtp_config[:send_email] if smtp_config[:send_email]
+  end
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
